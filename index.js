3d734a1d3d63e2bdc3082b54deeaa347c6403e84
Sydney Barlow <sydneybarlow44@gmail.com>
2018-03-22 22:19:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log(string)
+  console.log('HELLO')
   return string.toUpperCase()
 }
 function logWhisper(string) {
