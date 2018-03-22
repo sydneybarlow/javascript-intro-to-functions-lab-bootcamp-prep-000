4b6bfd77fd1c8583a449f1af2f6be2abff6d9b31
Sydney Barlow <sydneybarlow44@gmail.com>
2018-03-22 22:12:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function sayHiToGrandma(string) {
  console.log("I can\'t hear you!")
  if (string.toUpperCase() === string)
  console.log("YES INDEED!")
- else if (string )
+ else if (string.mixedCase())
  console.log('I love you, too.')
 }
