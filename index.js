653ee2a800027c610a7a659d46ce6e02efa71fa2
Sydney Barlow <sydneybarlow44@gmail.com>
2018-03-22 22:13:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
  if (string.toLowerCase() === string)
  console.log("I can\'t hear you!")
-}
- if (string.toUpperCase() === string)
+} if (string.toUpperCase() === string)
  console.log("YES INDEED!")
  else {
  console.log('I love you, too.')
