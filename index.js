0203c1df1f084af5d8d2daff27bc617ba836ed78
Sydney Barlow <sydneybarlow44@gmail.com>
2018-03-22 21:34:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+  return string.toUpperCase()
+}
+function logWhisper(string) {
+  console.log('hello')
+  return string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+ if ('hello')
+ console.log("I can\'t hear you!")
+ if ('HELLO')
+ console.log("YES INDEED!")
+ if ('I love you.')
+ console.log('I love you too.')
+}
